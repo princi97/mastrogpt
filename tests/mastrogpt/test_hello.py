b4e1cc09a7ec1013b323c5eba2b02f5818cfb5c2
princi97 <emanuele_principato@elmisoftware.com>
2025-01-31 17:41:27 +0000

    Hello, Emanuele

--- tests/mastrogpt/test_hello.py
+++ tests/mastrogpt/test_hello.py
@@ -7,5 +7,5 @@ def test_hello():
     assert res["output"] == "Hello, world"
     args = { "input": "Test"}
     res = hello.hello(args)
-    assert res["output"] == "Hello, Test"
+    assert res["output"] == "Hello, Emanuele"
     
